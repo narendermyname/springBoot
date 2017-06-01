1e8a72aa5871f2a977568848fb29b0d37a08be20
Narender Singh <narendermyname@gmail.com>
2017-06-01 23:25:42 +0530

    revamp code

--- src/main/java/com/naren/rest/Application.java
+++ src/main/java/com/naren/rest/Application.java
@@ -1,184 +1,40 @@
 package com.naren.rest;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
-
-import javax.persistence.Entity;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
-///@Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
 @EnableTransactionManagement
+@PropertySource({"classpath:db.properties"})
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/nstanwar");
-		dataSource.setUsername( "root" );
-		dataSource.setPassword( "root" );
-		return dataSource;
-	}
-
-	@Bean
-	public EntityManagerFactory entityManagerFactory() {
-
-		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		vendorAdapter.setGenerateDdl(true);
-
-		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		factory.setJpaVendorAdapter(vendorAdapter);
-		factory.setPackagesToScan("com.naren");
-		factory.setDataSource(dataSource());
-		factory.afterPropertiesSet();
-		factory.setJpaProperties(additionalProperties());
-
-		return factory.getObject();
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-
-		JpaTransactionManager txManager = new JpaTransactionManager();
-		txManager.setEntityManagerFactory(entityManagerFactory());
-		return txManager;
-	}
-
-	Properties additionalProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("show_sql", "true");
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		return properties;
-	}
 }
 
-@RestController
-class RestPersonResource{
-	@Autowired
-	private PersonRepository personRepo;
-	@RequestMapping("personList")
-	public Collection<Person> getPersonList(){
-		Person p=new Person();
-		p.setId(1001);
-		personRepo.delete(p);
-		return personRepo.findAll();
-	}
-	@RequestMapping("add")
-	public String add(){
-		Person p=new Person();
-		p.setId(100000);
-		p.setName("sddd");
-		return "SUCCESS";
-	}
-}
 @Component
+@Order(value=1)
 class PersonComponent implements CommandLineRunner{
-
 	@Autowired
 	private PersonRepository personRepo;
 	@Override
 	public void run(String... arg0) throws Exception {
-		Person p=new Person();
-		p.setId(1001);
-		personRepo.delete(p);
+		/*Person p=new Person("Lora");
+		personRepo.save(p);*/
 		for(Person person :personRepo.findAll()){
 			System.out.println(person.toString());
 		}
-		
 	}
-	
-}
-
-@Repository
-interface PersonRepository extends JpaRepository<Person, Long> {
-	
-	
-	List<Person> findAll();
-	
-
-}
-
-
-@Entity
-class Person{
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	private String name;
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;	
-	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
-	}
-	public Person(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-	public Person() {
-		super();
-	}
-
 }
--- src/test/java/com/naren/test/ApplicationTest.java
+++ src/test/java/com/naren/test/ApplicationTest.java
@@ -3,13 +3,23 @@
  */
 package com.naren.test;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.naren.rest.Person;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import com.naren.rest.Application;
 
@@ -17,26 +27,33 @@ import com.naren.rest.Application;
  * @author narender
  *
  */
+@Configuration
 public class ApplicationTest {
 
-	/**
-	 * 
-	 */
-	public ApplicationTest() {
-		// TODO Auto-generated constructor stub
-	}
-
+	private static final RestTemplate templet =  new RestTemplate();
+	private static final String PERSON_RESOURCE = "http://localhost:8080/person";
+	
 	/**
 	 * @param args
 	 */
-	//@Test
 	public static void main(String[] args) {
-		ApplicationContext context=new AnnotationConfigApplicationContext(Application.class);
+		/*ApplicationContext context=new AnnotationConfigApplicationContext(Application.class);
 		EntityManagerFactory emf=context.getBean(EntityManagerFactory.class);
 		EntityManager em=emf.createEntityManager();
-		Query query=em.createNativeQuery("SELECT * FROM Person");
-		System.out.println(query.getResultList());
-		
+		Query query=em.createQuery("SELECT p FROM Person p");
+		query.getResultList().stream().forEach(e -> System.out.println(e));*/
+		//getPersons();
+		postPerson();
+	}
+	
+	private static void getPersons(){
+		List<Person> p1 = templet.getForObject(PERSON_RESOURCE, List.class);
+		//p1.stream().forEach(p -> System.out.println(p1));
+	}
+	
+	private static void postPerson(){
+		ResponseEntity<Person> response = templet.postForEntity(PERSON_RESOURCE, new Person("Laxmi Shekhawat"), Person.class);
+		System.out.println(response.getBody().toString());
 	}
 
 }
